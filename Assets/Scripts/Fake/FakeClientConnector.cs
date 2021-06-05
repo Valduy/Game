d0c571c1b381b4ef6b23f458157e01586baa582b
Valduy <whnpir@gmail.com>
2021-06-05 22:33:40 +0300

    добавил фейковые сцены для имитации соединения

---
+++ Assets/Scripts/Fake/FakeClientConnector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Assets.Scripts.UI.Loading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Fake
+{
+    public class FakeClientConnector : MonoBehaviour
+    {
+        void Start()
+        {
+            MatchData.UdpClient = new UdpClient(54321);
+            MatchData.Clients = new List<IPEndPoint> {new IPEndPoint(IPAddress.Loopback, 54322)};
+            SceneManager.LoadScene("Client");
+        }
+    }
+}
---
+++ Assets/Scripts/Fake/FakeHostConnector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Assets.Scripts.UI.Loading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Fake
+{
+    public class FakeHostConnector : MonoBehaviour
+    {
+        void Start()
+        {
+            MatchData.UdpClient = new UdpClient(54322);
+            MatchData.Clients = new List<IPEndPoint> { new IPEndPoint(IPAddress.Loopback, 54321) };
+            SceneManager.LoadScene("Host");
+        }
+    }
+}
