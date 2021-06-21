13565730c69fbc0d05da2bf558516ad75a5b4710
Kikuzero <vovaazarov@gmail.com>
2021-06-22 01:28:02 +0300

    Добавлены таймауты и исправлен баг удара

---
+++ Assets/Scripts/Tests/DirectionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+using Assets.Scripts.ECS.Components;
+using Assets.Scripts.ECS.Systems;
+
+namespace Tests
+{
+    public class DirectionScript
+    {
+        [Test]
+        public void DirectionScriptSimplePasses()
+        {
+            
+            // Use the Assert class to test conditions
+        }
+    }
+}
